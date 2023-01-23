8879517cda3b1d91da0c3ead2b5f5dbac1af273d
nick <ntegan1@gmail.com>
2023-01-22 19:26:28 -0500

    start cache directory and download categories
    directory to store download responses
    
    start downloading categories
    
    fix
    
    vod not movie

--- include/xuiapi.sh
+++ include/xuiapi.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
 function authenticate_request() {
   providerurl="$1"
   username="$2"
   password="$3"
   player_api_request "${providerurl}" "${username}" "${password}" ""
+}
+function download_categories_for_stream_type() {
+  providerurl="$1"
+  username="$2"
+  password="$3"
+  stream_type="$4"
+  extraparams="&action=get_${stream_type}_categories"
+  player_api_request "${providerurl}" "${username}" "${password}" "${extraparams}"
 }
 function player_api_request() {
   numparams="$#"
